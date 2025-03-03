e390440b37ccc9fb0f60b5c8a4b4440d2d019a94
daniel <20106595@mail.wit.ie>
2025-03-03 18:01:46 +0000

    Add blood cell images for testing

--- src/main/java/daniels_nagornuks/App.java
+++ src/main/java/daniels_nagornuks/App.java
@@ -12,9 +12,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), 1800, 900);
         stage.setScene(scene);
-        stage.setMaximized(true);
         stage.show();
     }
 
